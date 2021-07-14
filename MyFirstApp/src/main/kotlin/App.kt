1648ba01240dcd5f20a44004f18f33de649f1087
Isaac Miti <ikayz@live.com>
2021-07-14 11:41:34 +0200

    Use an if expression

--- MyFirstApp/src/main/kotlin/App.kt
+++ MyFirstApp/src/main/kotlin/App.kt
@@ -3,5 +3,9 @@ Created by IntelliJ IDEA. User: ikayz Date: 09/07/2021
 Day: Friday Time: 7:19 pm
 */
 fun main(args: Array<String>) {
-    println("Pow!")
+    var x = 3
+    var y = 1
+
+    println(if (x > y) "x is greater than y" else "x is not greater than y")
+    println("This line runs no matter what")
 }
