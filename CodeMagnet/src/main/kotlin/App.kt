349179d6709088e3dc1fdc56880a11d9c13ad280
Isaac Miti <ikayz@live.com>
2021-07-14 12:16:36 +0200

    Complete code magenet exercise

---
+++ CodeMagnet/src/main/kotlin/App.kt
@@ -0,0 +1,14 @@
+/*
+Created by IntelliJ IDEA. User: ikayz Date: 14/07/2021 
+Day: Wednesday Time: 11:45 am
+*/
+fun main(args: Array<String>) {
+    var x = 1
+
+    while (x < 3) {
+        print(if (x == 1) "Yab" else "Dab")
+        print("ba")
+        x = x + 1
+    }
+    if (x == 3) println("Do")
+}
