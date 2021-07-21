e983b250f80dff44115218af0f5cb7656852f349
Isaac Miti <ikayz@live.com>
2021-07-21 16:39:33 +0200

    Add data types

---
+++ DataTypes/src/main/kotlin/App.kt
@@ -0,0 +1,24 @@
+/*
+Created by IntelliJ IDEA. User: ikayz Date: 21/07/2021 
+Day: Wednesday Time: 9:40 am
+*/
+fun main(args: Array<String>) {
+    // Byte
+    var byte: Byte = 1
+
+    // Short
+    var short: Short = 12
+
+    // Int
+    var int: Int = 123
+
+    // Long
+    var long: Long = 1234L
+
+    println("Byte: $byte")
+    println("Short: $short")
+    println("Int: $int")
+    println("Long: $long")
+
+    //
+}
