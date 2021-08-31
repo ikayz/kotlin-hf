c03c23f3c3fc816e6e1aabaaa0abc22fb8f966a1
Isaac Miti <ikayz@live.com>
2021-08-31 12:10:52 +0200

    Add phrase o matic

---
+++ Scratch/src/main/kotlin/Greetings.kt
@@ -0,0 +1,4 @@
+/*
+Created by IntelliJ IDEA. User: ikayz Date: 25/08/2021 
+Day: Wednesday Time: 12:06 pm
+*/
---
+++ Scratch/src/main/kotlin/PracticeTimeOne.kt
@@ -0,0 +1,6 @@
+/*
+Created by IntelliJ IDEA. User: ikayz Date: 26/08/2021 
+Day: Thursday Time: 9:55 am
+*/
+class PracticeTimeOne {
+}
