4e38dd3dea4268553a951bb015eeadb7aeb4004e
Isaac Miti <ikayz@live.com>
2021-08-31 12:16:42 +0200

    Add array size

--- PhraseOMatic/src/main/kotlin/PhraseOMatic.kt
+++ PhraseOMatic/src/main/kotlin/PhraseOMatic.kt
@@ -3,5 +3,9 @@ fun main(args: Array<String>) {
     val wordArray2 = arrayOf("empowered", "leveraged", "aligned", "targeted")
     val wordArray3 = arrayOf("process", "paradigm", "solution", "portal", "vision")
 
+    val arraySize1 = wordArray1.size
+    val arraySize2 = wordArray2.size
+    val arraySize3 = wordArray3.size
+
 
 }
