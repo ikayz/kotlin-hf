7154f9e68995d6bdcea198de25c0c6ef9f5ebe2e
Isaac Miti <ikayz@live.com>
2021-09-07 23:18:23 +0200

    Add message variable

---
+++ NoClasses/src/main/kotlin/main.kt
@@ -0,0 +1,31 @@
+/*
+Created by IntelliJ IDEA. User: ikayz Date: 31/08/2021 
+Day: Tuesday Time: 10:34 pm
+*/
+// Kotlin from Pluralsight
+
+fun main(args: Array<String>) {
+    var q: Question = Question()
+    q = Question()
+    q.Answer = "42"
+
+    q.display()
+
+    println("The answer to the question \"${q.Question}\" is \"${q.Answer}\"")
+
+    val message = if (q.Answer == q.CorrectAnswer) {
+        println("You were correct")
+    } else {
+        println("Try again?")
+    }
+}
+
+class Question {
+    var Answer:String = ""
+    var CorrectAnswer = "42"
+    val Question: String = "What is the answer to life, the universe and everything?"
+
+    fun display() {
+        println("You said $Answer")
+    }
+}
