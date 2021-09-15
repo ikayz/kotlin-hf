f27753ecc5a732577454a948630dc0c25683854d
Isaac Miti <ikayz@live.com>
2021-09-15 12:17:08 +0200

    Solve code magnet

---
+++ codemagnet-two/src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+/*
+Created by IntelliJ IDEA. User: ikayz Date: 15/09/2021 
+Day: Wednesday Time: 11:50 am
+*/
+fun main(args: Array<String>) {
+    val index = arrayOf(1, 3, 4, 2)
+    val fruit = arrayOf("Apple", "Banana", "Cherry", "Blueberry", "Pomegranate")
+    var x = 0
+    var y: Int
+
+    while (x < 4) {
+        y = index[x]
+        println("Fruit = ${fruit[y]}")
+        x = x + 1
+    }
+}
